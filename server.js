91f8f2546e4cfd9467b2356b9499866607d431b3
Timothy Rodriguez <tim.rdrgz2017@live.com>
2023-12-14 17:59:01 -0600

    json showing in browser

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ const app = express()
 
 const port = 3000
 
+const drinks = require("./models/drinks.js")
 
 app.get("/", (req, res) => {
     res.send(`Welcome to the Gitpub App!`)
 })
 
+app.get("/drinks", (req, res) => {
+    res.send(drinks)
+})
 
 
 
