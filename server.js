0531ca5d3e897442e937ffd9053f711449d34ddb
Timothy Rodriguez <tim.rdrgz2017@live.com>
2023-12-14 17:53:31 -0600

    Welcome message displayed

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+const port = 3000
+
+
+app.get("/", (req, res) => {
+    res.send(`Welcome to the Gitpub App!`)
+})
+
+
+
+
+
+
+app.listen(port, () => {console.log("is this thing really on?")})
